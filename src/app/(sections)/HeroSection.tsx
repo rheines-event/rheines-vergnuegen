76b687a5870d5279e3c4afecac8b440fad5bc7e6
Oskar Herz <oskar.herz@convalid-ai.de>
2023-08-30 22:10:03 +0200

    🐛 fix: fixes Hero-section video autoplay on Safari

--- src/app/(sections)/HeroSection.tsx
+++ src/app/(sections)/HeroSection.tsx
@@ -11,6 +11,7 @@ export function HeroSection() {
         loop
         muted
         autoPlay
+        playsInline
         className="absolute h-full w-full object-cover inset-0 -z-10 brightness-50">
         <source src="/hero.mp4" type="video/mp4" />
       </video>
